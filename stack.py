f99f4b81f52a7cb66e82fb9a8ae1766a4fb14d40
TheRealArnav <tyagi.arnav02@gmail.com>
2026-02-22 14:57:23 +0100

    Add files via upload

--- stack.py
+++ stack.py
@@ -27,12 +27,28 @@ class Stack:
     
     def display(self):
         print(self.stack)
+
+    def type(self,word):
+        global text
+        self.word = word
+        text = text + word
+        self.stack.append(word)
+
+    def undo(self):
+        self.stack.pop(-1)
+        global text
+        text = ""
+        for i in self.stack:
+            text = text+i
+
+
+
     
 
 
-stack1 = Stack(6)
+stack1 = Stack(10000)
 
-x = input("Enter a word")
+'''x = input("Enter a word")
 
 for i in x:
     stack1.push(i)
@@ -43,4 +59,39 @@ for i in range(len(stack1.stack),0,-1):
     res = res + stack1.stack[-1]
     stack1.pop()
 
-print(res)    
+print(res)'''
+
+
+'''x = input("Enter a word")
+
+for i in x:
+    stack1.push(i)
+
+res = str()
+
+for i in range(len(stack1.stack),0,-1):
+    res = res + stack1.stack[-1]
+    stack1.pop()
+
+
+
+if res == x:
+    print("Word is a palindrome")
+else: 
+    print("word is not palindrome")'''
+
+stackhistory = Stack(100)
+text = ""
+x = input("ENter a word")
+stackhistory.type(x)
+
+y = input("enter a word")
+
+stackhistory.type(y)
+print(stackhistory.stack)
+
+stackhistory.undo()
+
+print(stackhistory.stack)
+print(text)
+
